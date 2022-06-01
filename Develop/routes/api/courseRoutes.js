e36a592695982e3606dac2370dc8473effc99696
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-05-31 21:28:01 -0500

    Sending to GitHub for teammates to fork.

---
+++ Develop/routes/api/courseRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {
+  getCourses,
+  getSingleCourse,
+  createCourse,
+  updateCourse,
+  deleteCourse,
+} = require('../../controllers/courseController.js');
+
+// Get all courses works. api/courses/
+
+// /api/courses
+router.route('/').get(getCourses).post(createCourse);
+
+// /api/courses/:courseId
+router
+  .route('/:courseId')
+  .get(getSingleCourse)
+  .put(updateCourse)
+  .delete(deleteCourse);
+
+module.exports = router;
